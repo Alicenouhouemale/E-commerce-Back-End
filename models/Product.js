8b374fa9a182c1d8eb4b368b881f1891ee67c48d
Alice Nouhouemale <alicenouhouemale@gmail.com>
2022-12-03 16:42:13 -0800

    update

--- models/Product.js
+++ models/Product.js
@@ -14,7 +14,7 @@ Product.init(
     //   product_name: "Basketball",
     //   price: 200.00,
     //   stock: 3,
-    //   tagIds: [1, 2, 3, 4]//??
+    //   tagIds: [1, 2, 3, 4]
     // }
     id: {
       type: DataTypes.INTEGER,
